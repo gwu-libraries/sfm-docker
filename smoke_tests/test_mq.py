2e1be50549493231a50bd340ce9d688bf36e3318
Justin Littman <justinlittman@gwu.edu>
2015-10-21 13:17:06 -0400

    Unskipping smoke tests.

--- smoke_tests/test_mq.py
+++ smoke_tests/test_mq.py
@@ -10,8 +10,6 @@ class MqSmokeTest(unittest.TestCase):
                          os.environ.get("MQ_ENV_RABBITMQ_DEFAULT_PASS"))
         self.assertTrue(self.cl.is_alive())
 
-    #TODO: Unskip once component that creates sfm_change available.
-    @unittest.skip("Skipped until component available.")
     def test_exchange(self):
         exchanges = self.cl.get_exchanges()
         for exchange in exchanges:
@@ -20,8 +18,6 @@ class MqSmokeTest(unittest.TestCase):
         else:
             self.assertTrue(False, "Exchange not found.")
 
-    #TODO: Unskip once flickr_harvester available.
-    @unittest.skip("Skipped until flickr_harvester available.")
     def test_queues(self):
         queues = self.cl.get_exchanges()
         queues_names = {queue["name"] for queue in queues}
