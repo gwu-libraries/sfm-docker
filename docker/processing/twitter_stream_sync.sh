6cd0fbebd844b606d8103304775645561c55012c
Justin Littman <justinlittman@gwu.edu>
2018-05-24 11:48:05 -0400

    refs sfm 926. Additions / upgrades to processing container.

---
+++ docker/processing/twitter_stream_sync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e 
+set -o pipefail
+
+echo "Starting export $1 to $2 (`date`)"
+cd $2
+find_warcs.py $1 | tr ' ' '\n' > source.lst
+cat source.lst | xargs basename -a | sed 's/.warc.gz/.json.gz/' > dest_json.lst
+cat source.lst | xargs basename -a | sed 's/.warc.gz/.tweet_id.txt/' > dest_txt.lst
+export SHELL=/bin/bash
+parallel -j $3 -a source.lst -a dest_json.lst -a dest_txt.lst --xapply "[ -f {2} ] || twitter_stream_warc_iter.py {1} | tee >(gzip > {2}) | jq -r '.id_str' > {3}"
+echo "Done export $1 to $2 (`date`)"
