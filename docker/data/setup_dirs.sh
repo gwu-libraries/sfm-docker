197658c3aa236c5396b656a62db48012f8c9ce2f
SvenLieber <sven.lieber@ugent.be>
2021-01-23 14:02:17 +0100

    adapted directories for more fine-granular use

--- docker/data/setup_dirs.sh
+++ docker/data/setup_dirs.sh
@@ -2,21 +2,21 @@
 
 groupadd -r sfm --gid=$SFM_GID && useradd -r -g sfm --uid=$SFM_UID sfm
 
-export COLLECTION_SET_DIR=/sfm-data/collection_set
+export COLLECTION_SET_DIR=/sfm-collection-set-data/collection_set
 if [ ! -d $COLLECTION_SET_DIR ]; then
     echo "Creating collection_sets directory"
     mkdir -p $COLLECTION_SET_DIR
     chown sfm:sfm $COLLECTION_SET_DIR
 fi
 
-export CONTAINERS_DIR=/sfm-data/containers
+export CONTAINERS_DIR=/sfm-containers-data/containers
 if [ ! -d $CONTAINERS_DIR ]; then
     echo "Creating containers directory"
     mkdir -p $CONTAINERS_DIR
     chown sfm:sfm $CONTAINERS_DIR
 fi
 
-export EXPORT_DIR=/sfm-data/export
+export EXPORT_DIR=/sfm-export-data/export
 if [ ! -d $EXPORT_DIR ]; then
     echo "Creating export directory"
     mkdir -p $EXPORT_DIR
